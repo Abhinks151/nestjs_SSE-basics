f9838ff337091e3784672d0a98a846ef52d6850d
Abhin <abhin1511203@gmial.com>
2026-06-20 22:47:29 +0530

    feat: sample SSE project

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,8 +2,9 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  async getData() {
+  async getData(){
     const reposnse = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum&vs_currencies=usd');
+
     const data = await reposnse.json();
     return data;
   }
